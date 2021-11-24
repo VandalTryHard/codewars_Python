59cbb9b46b5a9423dd46c09a94246ac408b9d2d5
Gulenok Valentin <gulenok.valentin@yandex.ru>
2021-11-24 20:43:33 +0600

    solution codewars.com Python
    8 kyu
    7 kyu
    6 kyu
    5 kyu

---
+++ 5 kyu/Moving_Zeros_To_The_End.py
@@ -0,0 +1,25 @@
+"""5 kyu
+Moving Zeros To The End
+Write an algorithm that takes an array and moves all of the zeros to the end, 
+preserving the order of the other elements.
+move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]"""
+
+# def move_zeros(array):
+#     for i in array:
+#         if i == 0:
+#             array.remove(i)
+#             array.append(i)
+#         else:
+#             continue
+#     return array
+
+# How this works
+array = [1, 0, 1, 2, 0, 1, 3]
+for i in array:
+    if i == 0:
+        array.remove(i)
+        array.append(i)
+    else:
+        continue
+
+print(array)
