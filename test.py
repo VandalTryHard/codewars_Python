444298c63ac1483cbe26ca8f2bfd5878ac1dd686
Gulenok Valentin <gulenok.valentin@yandex.ru>
2021-12-31 01:42:27 +0600

    codewars_Python
    8 kyu
    7 kyu
    6 kyu
    5 kyu

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-x = 3
-print(x // 3)
+limit = 8
+a, b = 1, 1
+total = 0
+while a <= limit:
+    if a % 2 == 0:
+        total += a
+    a, b = b, a+b  # the real formula for Fibonacci sequence
+print(total)
