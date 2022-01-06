4fc40a4989107ec2f8ec11a62995fe647e0e41ce
Gulenok Valentin <gulenok.valentin@yandex.ru>
2022-01-06 20:17:47 +0600

    solution codewars.com Python
    8 kyu
    7 kyu
    6 kyu
    5 kyu

--- 6 kyu/CountingDuplicates.py
+++ 6 kyu/CountingDuplicates.py
@@ -23,17 +23,17 @@ new_text = new_text.lower()
 count = 0
 dictionary = {}
 
-for i in range(0,len(new_text)):
+for i in range(0, len(new_text)):
     dictionary[new_text[i]] = 0
 
 for key in dictionary:
-    for i in range(0,len(new_text)):
-        if(key == new_text[i]):
+    for i in range(0, len(new_text)):
+        if (key == new_text[i]):
             dictionary[key] = dictionary[key] + 1
 
 for key in dictionary:
-    if(dictionary[key] > 1):
+    if (dictionary[key] > 1):
         count = count + 1
 
 print(dictionary)
-#return count
+# return count
