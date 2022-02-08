9aa7d43addbc4074be84129e8ee58eae4bea0a67
Gulenok Valentin <gulenok.valentin@yandex.ru>
2022-02-08 21:00:42 +0600

    solution codewars.com Python
    8 kyu
    7 kyu
    6 kyu
    5 kyu

---
+++ 5 kyu/Integers_Recreation_One.py
@@ -0,0 +1,79 @@
+"""5 kyu
+Integers: Recreation One
+1, 246, 2, 123, 3, 82, 6, 41 are the divisors of number 246. Squaring these divisors we get: 1, 60516, 4, 15129, 9,
+6724, 36, 1681. The sum of these squares is 84100 which is 290 * 290.
+Task
+
+Find all integers between m and n (m and n integers with 1 <= m <= n) such that the sum of their squared divisors is
+itself a square.
+
+We will return an array of subarrays or of tuples (in C an array of Pair) or a string. The subarrays
+(or tuples or Pairs) will have two elements: first the number the squared divisors of which is a square and then the sum
+ of the squared divisors.
+Example:
+
+list_squared(1, 250) --> [[1, 1], [42, 2500], [246, 84100]]
+list_squared(42, 250) --> [[42, 2500], [246, 84100]]
+
+The form of the examples may change according to the language, see "Sample Tests".
+Note
+
+In Fortran - as in any other language - the returned string is not permitted to contain any redundant trailing
+whitespace: you can use dynamically allocated character strings.
+Fundamentals
+Algorithms
+Optimization"""
+
+"""
+CACHE = {}
+
+
+def squared_cache(number):
+    if number not in CACHE:
+        divisors = [x for x in range(1, number + 1) if number % x == 0]
+        CACHE[number] = sum([x * x for x in divisors])
+        return CACHE[number]
+
+    return CACHE[number]
+
+
+def list_squared(m, n):
+    ret = []
+
+    for number in range(m, n + 1):
+        divisors_sum = squared_cache(number)
+        if (divisors_sum ** 0.5).is_integer():
+            ret.append([number, divisors_sum])
+
+    return ret
+"""
+
+# How this works
+def main():
+    print(list_squared(1, 250))
+
+
+CACHE = {}
+
+
+def squared_cache(number):
+    if number not in CACHE:
+        divisors = [x for x in range(1, number + 1) if number % x == 0]
+        CACHE[number] = sum([x * x for x in divisors])
+        return CACHE[number]
+
+    return CACHE[number]
+
+
+def list_squared(m, n):
+    ret = []
+
+    for number in range(m, n + 1):
+        divisors_sum = squared_cache(number)
+        if (divisors_sum ** 0.5).is_integer():
+            ret.append([number, divisors_sum])
+
+    return ret
+
+
+main()
